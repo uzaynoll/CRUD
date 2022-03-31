57716e2b8d86aa87fabda9c919367cf94763d99c
Ujjwal Khanal <litl.prince1@gmail.com>
2022-03-31 14:34:43 +0545

    V 1.1.5

--- src/context/NoteContext.js
+++ src/context/NoteContext.js
@@ -1,19 +1,22 @@
-import createDataContext from './createDataContext';
+import createDataContext from "./createDataContext";
 
 const reducer = (state, action) => {
-    switch(action.type) {
-        case 'addNote': 
-            return ([...state, { title: `Note #${state.length + 1}`}]);
+  switch (action.type) {
+    case "addNote":
+      return [...state, { title: `Note #${state.length + 1}` }];
 
-        default:
-            return (state);
-    }
+    default:
+      return state;
+  }
 };
-const addNotes = dispatch => {
-    return () => {
-        dispatch({type: 'addNote'});
-    }
-     
+const addNotes = (dispatch) => {
+  return () => {
+    dispatch({ type: "addNote" });
+  };
 };
 
-export const { Context, Provider } = createDataContext( reducer, { addNotes }, []);
+export const { Context, Provider } = createDataContext(
+  reducer,
+  { addNotes },
+  []
+);
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,19 +1,26 @@
-import React ,{ useContext } from 'react';
-import {Text, View, StyleSheet,FlatList, Button } from 'react-native';
-import { Context } from '../context/NoteContext';
+import React, { useContext } from "react";
+import { Text, View, StyleSheet, FlatList, Button } from "react-native";
+import { Context } from "../context/NoteContext";
 
 const IndexScreen = () => {
-    const {state, addNotes} = useContext(Context);
+  const { state, addNotes } = useContext(Context);
 
-    return <View>
-        {/* <Text> Index Screen.</Text> */}
-        <Button title='Add a new Note' onPress={addNotes} />
-        <FlatList 
-            data={state}
-            keyExtractor={ (Note) => (Note.title)}
-            renderItem = {({item}) => { return <Text>{item.title}</Text>}}
-        />
+  return (
+    <View>
+      <Button title="Add a new Note" onPress={addNotes} />
+      <FlatList
+        data={state}
+        keyExtractor={(Note) => Note.title}
+        renderItem={({ item }) => {
+          return (
+            <View>
+              <Text>{item.title}</Text>
+            </View>
+          );
+        }}
+      />
     </View>
+  );
 };
 
 const styles = StyleSheet.create({});
